395f652db66a77ac9c578c3e76e796f805ffdfa4
Schm000L <darklord_dragonslayer@hotmail.com>
2018-09-24 23:35:45 +0200

    Embryo for graph generation

--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -31,8 +31,74 @@ const Header = () => {
 }
 
 export default class MainPage extends React.Component<{}, {}> {
+    n: number = Math.floor(Math.random() * 8) + 3;
+    i: number = 0;
+    p1Edges: number[][] = new Array;
+    p2Edges: number[][] = new Array;
+    p1Points: number = 0;
+    p2Points: number = 0;
+    p1sTurn: boolean = true;
 
+    nodes = this.createNodes(this.n);
+    edges = this.populateTree(this.n);
+    //console.log("eLen: " + edges.length);
 
+    private createNodes(n:number):number[] {
+        let nodes: number[]=[];
+        for(let i:number=0; i<n; i++) {
+            nodes.push(i);
+        }
+        return nodes;
+    }
+
+    // TODO: Skriv roligare generering av träd (ej kompletta träd.)
+    private populateTree(n:number) {
+        let edges: number[][]=new Array();
+        for(let i: number=0; i<n;i++) {
+            for(let j:number=0;j<n;j++) {
+                if(i<j) {
+                    let edge: [number, number, number] = [i, j, Math.floor(Math.random()*6)+1];
+                    edges.push(edge);
+                }
+            }
+        }
+        return edges;
+    }
+
+    /*private addTakenNodes(n:number, add:number, takenNodes:number[]): number[] {
+        let alreadyExist: boolean = false;
+
+        for(n of takenNodes) {
+            if(add === n)
+                alreadyExist = true;
+        }
+
+        if(!alreadyExist)
+            takenNodes.push(add);
+        
+        return takenNodes;
+    }*/
+
+    /*private complete(n:number, nodes:number[], p1Edges:number[][], p2Edges:number[][]): boolean {
+        let returnValue: boolean = true;
+        let takenNodes:  number[] = new Array();
+        let takenEdges: number[][] = p1Edges.concat(p2Edges);
+        //console.log("Taken: " + takenEdges);
+        for(let edge of takenEdges) {
+            takenNodes = this.addTakenNodes(n, edge[0], takenNodes);//borde vara överflödig
+            takenNodes = this.addTakenNodes(n, edge[1], takenNodes);
+        }
+
+        console.log("takenNodes length: " + takenNodes.length);
+        for(n of nodes) {
+            if(takenNodes.indexOf(n) === -1) {
+                //console.log(n + "was still free!");
+                returnValue = false;
+            }
+        }
+        //console.log("Complete returned: " + returnValue);
+        return returnValue;
+    }*/
     
     render(){
         return(
